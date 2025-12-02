f41a2f1b430af37995073b8bed05c370ef19ff75
habibur-rahman-tanvir <habibur353637@gmail.com>
2025-12-02 17:17:34 +0600

    Add express-rate-limit for rate limiting on signup and signin routes; create rateLimit middleware

---
+++ src/middleware/rateLimit.js
@@ -0,0 +1,15 @@
+import rateLimit from 'express-rate-limit';
+
+const setRateLimit = (limit = 100, minute = 15, message = 'Rate limit reached') => {
+  console.log();
+  return rateLimit({
+    windowMs: 1000 * 60 * minute,
+    max: limit,
+    message: {
+      status: 'fail',
+      message,
+    },
+  });
+};
+
+export default setRateLimit;
--- src/routes/authRouter.js
+++ src/routes/authRouter.js
@@ -2,11 +2,12 @@ import express from 'express';
 import signupController from '../controller/signupController.js';
 import signinController from '../controller/signinController.js';
 import signoutController from '../controller/signoutController.js';
+import setRateLimit from '../middleware/rateLimit.js';
 
 const router = express.Router();
 
-router.post('/signup', signupController);
-router.post('/signin', signinController);
+router.post('/signup', setRateLimit(5, 60), signupController);
+router.post('/signin', setRateLimit(3, 12), signinController);
 router.post('/signout', signoutController);
 
 export default router;
