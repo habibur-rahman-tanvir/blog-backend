50a46e56081537ba799fcb3b48aef6f60510f5ca
habibur-rahman-tanvir <habibur353637@gmail.com>
2025-12-04 21:44:39 +0600

    Fix capitalization in success message for user creation

--- src/controller/authController.js
+++ src/controller/authController.js
@@ -9,7 +9,7 @@ export const createUser = async (req, res) => {
   });
   res.status(201).json({
     status: 'success',
-    message: 'user created successfully',
+    message: 'User created successfully',
     userId: user._id,
   });
 };
